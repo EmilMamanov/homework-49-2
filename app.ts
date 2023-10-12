862b63ea848862962302185ecf3a9b0020f5590a
Mamanov Emil <emil.mamanov97@gmail.com>
2023-10-12 11:24:39 +0600

    добавлен файл app.ts

---
+++ app.ts
@@ -0,0 +1,16 @@
+function generateChessboard(size: number): string {
+    let chessboard = '';
+
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
+            if ((row + col) % 2 === 0) {
+                chessboard += '██';
+            } else {
+                chessboard += '  ';
+            }
+        }
+        chessboard += '\n';
+    }
+
+    return chessboard;
+}
